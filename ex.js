d59880a4ce3fc899f168b0d19374a6421ed37c17
biozob <influcid91@gmail.com>
2022-12-04 17:40:38 +0200

    first programming primer exercise

---
+++ ex.js
@@ -0,0 +1,30 @@
+// TODO: define addFavoriteBook(..) function
+
+// TODO: define printFavoriteBooks() function
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+function addFavoriteBook(bookName){
+    if (!bookName.includes('Great')){
+        favoriteBooks.push(bookName);
+    }
+};
+
+function printFavoriteBooks(){
+    let bookLength = favoriteBooks.length;
+    console.log("Favorite books: " + bookLength);
+    
+    for ( let book of favoriteBooks) {
+        console.log(book);
+    }
+}
+
+printFavoriteBooks();
+
+// TODO: print out favorite books
