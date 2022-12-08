4ddd05dd177895423b4dab39f7755f98d969d007
biozob <influcid91@gmail.com>
2022-12-08 22:17:54 +0200

    format

--- deep-javascript/types-exercises/coercion.js
+++ deep-javascript/types-exercises/coercion.js
@@ -1,31 +1,31 @@
 // TODO: write the validation functions
 function isValidName(name) {
-    if ( typeof name === 'string' && name.trim().length >= 3 ) {
+    if (typeof name === 'string' && name.trim().length >= 3) {
         return true;
-    } 
+    }
     return false;
 }
 
 function hoursAttended(attended, length) {
-	if (typeof attended == "string" && attended.trim() != "") {
-		attended = Number(attended);
-	}
+    if (typeof attended == "string" && attended.trim() != "") {
+        attended = Number(attended);
+    }
 
-	if (typeof length == "string" && length.trim() != "") {
-		length = Number(length);
-	}
+    if (typeof length == "string" && length.trim() != "") {
+        length = Number(length);
+    }
 
-	if (typeof attended == "number" &&
-		typeof length == "number" &&
-		attended <= length &&
-		attended >= 0 &&
-		length >= 0 &&
-		Number.isInteger(attended) &&
-		Number.isInteger(length)) {
-		return true;
-	}
+    if (typeof attended == "number" &&
+        typeof length == "number" &&
+        attended <= length &&
+        attended >= 0 &&
+        length >= 0 &&
+        Number.isInteger(attended) &&
+        Number.isInteger(length)) {
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 // tests:
